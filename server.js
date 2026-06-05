07cbb1f460e6ce4d439ff27dfa0ee88d09be73a3
andinalloldtales <274286994+andinalloldtales@users.noreply.github.com>
2026-06-04 23:03:05 -0500

    Add routes

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('Hello')
+const express = require('express')
+const app = express()
+
+// routing
+app.get('/', (req, res) => {
+    res.send(`Hello!`)
+})
+
+app.listen(5173, ()=> {
+    console.log(`Node API running is running on port 5173`)
+})

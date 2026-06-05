d2fda81a56e4787f8fb0ab23cba7782d76da9ff2
andinalloldtales <274286994+andinalloldtales@users.noreply.github.com>
2026-06-05 08:09:01 -0500

    Refactor Product Routes

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 require('dotenv').config({ path: './secrets.env' })
 const express = require('express')
 const mongoose = require('mongoose')
-const Product = require('./models/productModel')
 const app = express()
+const productRoute = require('./routes/productRoute')
+
 app.use(express.urlencoded({extended: false}))
 
 
 app.use(express.json())
 
 // routing
+
+app.use('/api', productRoute);
+
 app.get('/', (req, res) => {
     res.send(`Hello!`)
 })
@@ -17,63 +21,7 @@ app.get('/blog', (req, res) => {
     res.send(`Hey blog!`)
 })
 
-app.get('/products', async(req, res) => {
-    try {
-        const products = await Product.find({})
-        res.status(200).json(products)
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
 
-app.get('/products/:id', async (req, res) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product)
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-
-})
-
-app.post('/product', async(req, res) => {
-    try {
-        const product = await Product.create(req.body)
-        res.status(200).json(product)
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).json({ message: error.message })
-    }
-})
-
-app.put('/products/:id', async(req, res) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.findByIdAndUpdate(id, req.body);
-        if(!product){
-            return res.status(404).json({message: `cannot find any product with this ID ${id}`})
-        }
-        const updatedProduct = await Product.findById(id);
-        res.status(200).json(updatedProduct);
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-})
-
-app.delete('/products/:id', async(req, res) => {
-    try {
-        const {id} = req.params;
-        const product = await Product.findByIdAndDelete(id);
-        if(!product){
-            return res.status(404).json({message: `cannot find any product with this ID ${id}`})
-        }
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
-
-})
 
 
 const PORT = process.env.PORT || 5173

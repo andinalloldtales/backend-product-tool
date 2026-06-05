931954d99c5dafdeee5346b0eb19121a1327729c
andinalloldtales <274286994+andinalloldtales@users.noreply.github.com>
2026-06-05 00:35:31 -0500

    Fetch Product by Id

--- server.js
+++ server.js
@@ -16,12 +16,24 @@ app.get('/blog', (req, res) => {
     res.send(`Hey blog!`)
 })
 
-app.get('/product', async(req, res) => {
+app.get('/products', async(req, res) => {
     try {
-
-    } catch {error} {
-        res.status(500).json(product);
+        const products = await Product.find({})
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({message: error.message})
     }
+})
+
+app.get('/products/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+
 })
 
 app.post('/product', async(req, res) => {

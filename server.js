b1550dba3cf7f1330c9c902c7f8b2527dfb59021
andinalloldtales <274286994+andinalloldtales@users.noreply.github.com>
2026-06-05 00:42:59 -0500

    Update Data Function

--- server.js
+++ server.js
@@ -46,6 +46,21 @@ app.post('/product', async(req, res) => {
     }
 })
 
+app.put('products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        if(!product){
+            return res.status(404).json({message: `cannot find any product with this ID ${id}`})
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+
+
 
 mongoose.set("strictQuery", false)
 mongoose.connect(process.env.MONGODB_URI)

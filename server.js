2c0123fca370f591f6d676ad0d8d07bd52ee0226
andinalloldtales <274286994+andinalloldtales@users.noreply.github.com>
2026-06-04 23:22:38 -0500

    Incorporate Nodemon

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send(`Hello!`)
 })
 
+app.get('/blog', (req, res) => {
+    res.send(`Hey blog!`)
+})
+
 app.listen(5173, ()=> {
     console.log(`Node API running is running on port 5173`)
 })
